4b30cab1bca9dfc94db2998924f80680b5337be3
Ludmi-deu <ludmideu@gmail.com>
2024-06-12 20:53:59 -0300

    navbar

--- src/components/NavBarComponent.js
+++ src/components/NavBarComponent.js
@@ -1,19 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '../assets/styles/NavBar.css';
+import DropdownMenu from './MenuDesplegable';
 
-function NavbarComponent () {
+function NavbarComponent() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
+  const tipoPropiedadOptions = ['editar', 'nuevo']; // Opciones para Tipo de Propiedad
+
   return (
     <nav className="navbar">
       <ul>
         <a href="/" className="nav-item">Inicio</a>
-        <a href="/Tipos-propiedad" className="nav-item">Tipos de propiedad</a>
-        <a href="/Propiedades" className="nav-item">Propiedades</a>
-        <a href="/Reservas" className="nav-item">Reservas</a>
-        <a href="/Localidades" className="nav-item">Localidades</a>
-        <a href="/Inquilinos" className="nav-item">Inquilinos</a>
+        <a href="/tipos-propiedad" className="nav-item" onClick={toggleMenu}>Tipo de Propiedad</a>
+        <a href="/propiedades" className="nav-item">Propiedades</a>
+        <a href="/reservas" className="nav-item">Reservas</a>
+        <a href="/localidades" className="nav-item">Localidades</a>
+        <a href="/inquilinos" className="nav-item">Inquilinos</a>
       </ul>
+      {isOpen && <DropdownMenu options={tipoPropiedadOptions} />} {/* Renderiza el menú desplegable si isOpen es true */}
     </nav>
   );
 }
 
 export default NavbarComponent;
+
--- src/index.js
+++ src/index.js
@@ -1,19 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
 import '../src/assets/styles/logo.css';
 import HeaderComponent from './components/HeaderComponent';
 import FooterComponent from './components/FooterComponent';
 import NavbarComponent from './components/NavBarComponent';
+import TipoPropiedadPage from './pages/tipoPropiedad/TipoPropiedadPage';
+import NewTipoPropiedad from './pages/tipoPropiedad/NewTipoPropiedad'
+import EditTipoPropiedad from './pages/tipoPropiedad/EditTipoPropiedad';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <BrowserRouter>
     <HeaderComponent/>
-    <FooterComponent/>
+      <Routes>
+      <Route path="/tipo-propiedad/editar" element={<EditTipoPropiedad />} />
+      <Route path="/tipoPropiedad/nuevo" element={<NewTipoPropiedad />}/>
+      <Route path="/tipoPropiedad" element={<TipoPropiedadPage />}/>
+      </Routes>
+      <FooterComponent/>
+    </BrowserRouter>
   </React.StrictMode>
+
 );
 
 // If you want to start measuring performance in your app, pass a function
